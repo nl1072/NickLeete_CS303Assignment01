1a7e2e9f8429734d86109cd6f72fc47fa434d46e
nl1072 <nl1072@gmail.com>
2024-02-08 21:07:01 -0600

    Updated project files.

---
+++ functions.cpp
@@ -0,0 +1,57 @@
+#include "functions.h"
+
+using namespace std;
+
+void printArray(int arr[]) {
+	cout << "[ ";
+	for (int i = 0; i < sizeof(arr); i++)
+		cout << arr[i] << " ";
+	cout << "]" << endl;
+}
+
+int findIdxInArr(int arr[], int num)
+{
+	if (sizeof(arr) == 0)
+		return -1;
+
+	for (int i = 0; i < sizeof(arr); i++) {
+		if (arr[i] == num)
+			return i;
+	}
+	return -2;
+}
+
+int replaceAtIdx(int arr[], int idx, int newNum)
+{
+	if (sizeof(arr) == 0)
+		return -1;
+
+	int oldNum = arr[idx];
+	arr[idx] = newNum;
+	cout << "Replacing value " << oldNum << " at index " << idx << " with value " << newNum << endl;
+
+	return 0;
+}
+
+int resize(int arr[])
+{
+	return 0;
+}
+
+int resizeAndAddToEnd(int arr[], int newNum)
+{
+	return 0;
+}
+
+int replaceWithZero(int arr[], int idx)
+{
+	if (sizeof(arr) == 0)
+		return -1;
+	arr[idx] = 0;
+	return 0;
+}
+
+int deleteidx(int arr[], int idx)
+{
+	return 0;
+}
---
+++ functions.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+void printArray(int arr[]);
+
+int findIdxInArr(int arr[], int num);
+
+int replaceAtIdx(int arr[], int idx, int newNum);
+
+int resize(int arr[]);
+
+int resizeAndAddToEnd(int arr[], int newNum);
+
+int replaceWithZero(int arr[], int idx);
+
+int deleteidx(int arr[], int idx);
